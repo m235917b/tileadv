4788b8461a33c0e85d3f1db692193891e5b0679d
m235917b <marvinb235917@gmail.com>
2025-11-01 22:20:07 +0100

    add unit test environment catch

---
+++ tests/test1.cpp
@@ -0,0 +1,30 @@
+/*#define CATCH_CONFIG_MAIN
+#include "ecs/ecscontext.hpp"
+#include "thirdparty/catch_amalgamated.hpp"
+
+struct Position {
+  float x;
+  float y;
+};
+
+TEST_CASE("ECS Upsert adds and retrieves components") {
+  ECSContext ctx;
+
+  ctx.getCommandBuffer().enqueue<UpsertComponent>(
+      UpsertComponent{"entity1", Position{1.f, 2.f}});
+  ctx.getScheduler().update(0.f); // flush commands if required
+
+  bool found = false;
+  ctx.getStore().view<Position>([&](auto id, auto &pos) {
+    if (id == "entity1") {
+      REQUIRE(pos.x == Catch::Approx(1.f));
+      REQUIRE(pos.y == Catch::Approx(2.f));
+      found = true;
+    }
+  });
+  REQUIRE(found);
+}*/
+
+#include <thirdparty/catch_amalgamated.hpp>
+
+TEST_CASE("example test") { REQUIRE(1 + 1 == 2); }
